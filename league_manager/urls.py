5be02a3e811d2f3049277967e5c04c5d7c78f696
Giorgio Momigliano <gmomigliano@protonmail.com>
2025-08-26 01:17:41 +0200

    move django admin to under /scheduler/

--- league_manager/urls.py
+++ league_manager/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("scheduler/admin/", admin.site.urls),
     path("scheduler/", include("scheduler.urls")),
 ]
 
